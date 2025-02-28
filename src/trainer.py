13618f4f30ce7661e464d8a3dc1dc4572932e22c
morixxfoxdata <morixx.datascience@gmail.com>
2025-02-28 21:31:52 +0900

    gidc was installed

--- src/trainer.py
+++ src/trainer.py
@@ -8,6 +8,7 @@ from src.dataset.speckle_pred import speckle_pred_inv
 from src.utils.inv_recon import img_reconstruction
 from src.utils.utils import np_to_torch, min_max_normalize, total_variation_loss, image_save
 from src.models.linear import FCModel
+from src.models.GIDC import GIDC28
 # python -m src.trainer
 file_y = "Rand+Mnist+Rand_pix28x28_image(1500+10+1500)x2_sig2500x4wave.npz"
 file_x = "Rand+Mnist+Rand_size28x28_image(1500+10+1500)x2.npz"
@@ -70,6 +71,82 @@ def train_simple(collected_path, target_path, select):
                 with torch.no_grad():
                     reconstucted_target = (
                         model(y_).squeeze(0).squeeze(0).reshape(784))
+                    # print("再構成画像の shape:", reconstucted_target.shape)
+                mse_val, ssim_score, psnr = image_save(
+                    x=X_mnist[num, :],
+                    y=reconstucted_target.cpu().numpy(),
+                    epoch=epoch + 1,
+                    num=num,
+                    select=select,
+                    model=model.model_name,
+                    lr=lr,
+                    tv=TV_strength)
+                print(f"mse:{mse_val:.5f}, ssim:{ssim_score:.5f}, PSNR: {psnr:.5f}")
+        print(f"\n================ Image {num} の学習終了 ================\n")
+        recon_list.append(reconstucted_target.detach().cpu().numpy())
+    print("モデルトレーニングが完了しました。")
+    return recon_list
+
+
+
+def train_gidc(collected_path, target_path, select):
+# =============================================
+    num_epochs = 3000
+    lr = 0.02
+    TV_strength = 2e-9
+# =============================================
+    if torch.cuda.is_available():
+        device = "cuda"
+    elif torch.backends.mps.is_available():
+        device = "mps"
+    else:
+        device = "cpu"
+    print("Using device:", device)
+    Y_random, Y_mnist = collected_signal(path=collected_path, select=select)
+    X_random, X_mnist = target_image(path=target_path, select=select)
+    print("======================================")
+    print("Y_mnist shape:", Y_mnist.shape)
+    print("Y_random shape:", Y_random.shape)
+    print("X_mnist shape:", X_mnist.shape)
+    print("X_random shape:", X_random.shape)
+    print("X_mnist min, max:", X_mnist.min(), X_mnist.max())
+    print("X_random min, max:", X_random.min(), X_random.max())
+    print("======================================")
+    print("ランダムパターンからspeckle_patternsを推定します。pinvを利用します。")
+    S = speckle_pred_inv(path_x=target_path, path_y=collected_path, select=select)
+    print("speckle by random:", S.min(), S.max(), S.shape)
+    X_mnist_first = img_reconstruction(S, Y_mnist)
+    print("X_pinv:", X_mnist_first.min(), X_mnist_first.max(), X_mnist_first.shape)
+    print("======================================")
+    S_tensor = np_to_torch(S).float().to(device)
+    Y_mnist_tensor = np_to_torch(Y_mnist).float()
+    X_input_tensor = np_to_torch(X_mnist_first).float()
+    criterion = nn.MSELoss()
+    recon_list = []
+    for num in range(10):
+        print(f"\n================ Image {num} の学習開始 ================\n")
+        input = min_max_normalize(X_input_tensor[num].reshape((1, 1, 28, 28))).to(device)
+        y_ = Y_mnist_tensor[num].to(device)
+        # print(y_.shape)
+        model = GIDC28(kernel_size=5, name="gidc", select=select).to(device)
+        optimizer = optim.Adam(model.parameters(), lr=lr)
+        for epoch in range(num_epochs):
+            model.train()
+            optimizer.zero_grad()
+            output = model(input).reshape((1, 1, 28, 28))
+            tv = TV_strength * total_variation_loss(output)
+            Y_dash = torch.mm(output.reshape(1, 28**2), S_tensor)
+            loss = criterion(Y_dash, y_.unsqueeze(0)) + tv
+            loss.backward()
+            optimizer.step()
+            if (epoch + 1) % 500 == 0:
+                print(
+                f"Image {num}, Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.14f}"
+                )
+                model.eval()
+                with torch.no_grad():
+                    reconstucted_target = (
+                        model(input).squeeze(0).squeeze(0).reshape(784))
                     # print("再構成画像の shape:", reconstucted_target.shape)
                 mse_val, ssim_score, psnr = image_save(
                     x=X_mnist[num, :],
